dd93359ec03588cda43901e95506e7c2c1ff8c47
moonmagian <moonmagian@gmail.com>
2018-08-19 23:57:37 +0800

    Initial commit

---
+++ GxbManager.py
@@ -0,0 +1,173 @@
+from selenium import webdriver
+import selenium
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+import re
+STATUS_OUTPUT = \
+    '''Video: {0}
+Status: {1}
+Time(sec): {2} / {3}'''
+
+CLASS_REGEX = r'''https://bh3773.class.gaoxiaobang.com/class/(\d+)/unit/(\d+)/chapter/(\d+)'''
+CLASS_STRING = '''https://bh3773.class.gaoxiaobang.com/class/{0}/unit/{1}/chapter/{2}'''
+# Get VideoList needs LOTS OF resources, cache them to lower CPU usage.
+VLcache = {}
+# Also for VideoListIds
+VLIDcache = {}
+
+
+class Status:
+    title = "TITLE"
+    playStatus = "PLAYSTATUS"
+    ctime = -1
+    duration = -1
+    error = False
+
+    def __repr__(self):
+        if(not self.error):
+            return STATUS_OUTPUT.format(self.title, self.playStatus, str(self.ctime), str(self.duration))
+        else:
+            return "Not valid video page."
+
+
+def videoList(driver: webdriver.chrome.webdriver.WebDriver):
+    try:
+        cid = re.match(CLASS_REGEX, driver.current_url).groups()[0]
+        if(cid in VLcache.keys()):
+            return VLcache[cid]
+        else:
+            VLcache[cid] = list(filter(lambda x: x.get_attribute(
+                'content_type') == 'Video', driver.find_elements_by_class_name("chapter-info")))
+            return VLcache[cid]
+    except:
+        return []
+
+
+def autoLogin(driver: webdriver.chrome.webdriver.WebDriver, loginLink: str, username: str, passwd: str):
+    try:
+        driver.get(loginLink)
+        driver.find_element_by_id('username').send_keys(username)
+        driver.find_element_by_id('password').send_keys(passwd)
+        driver.find_element_by_class_name('login_btn').click()
+        return True
+    except selenium.common.exceptions.NoSuchElementException:
+        return False
+
+
+def status(driver: webdriver.chrome.webdriver.WebDriver):
+    '''
+    Get current status of video page.
+
+    :param driver: WebDriver, the WebDriver to get status
+    :returns: Status, a Status object storing status information
+    '''
+    output = Status()
+    try:
+        videoPlayer = driver.find_element_by_id('video_player_html5_api')
+        output.title = driver.find_element_by_class_name('chapter-title').text
+        videoShell = driver.find_element_by_id('video_player')
+        vsClass = videoShell.get_attribute('class')
+        if(vsClass.find('vjs-paused') + 1):
+            output.playStatus = 'paused'
+        else:
+            output.playStatus = 'playing'
+        output.duration = videoPlayer.get_property('duration')
+        output.ctime = videoPlayer.get_property('currentTime')
+    except selenium.common.exceptions.NoSuchElementException:
+        output.error = True
+    finally:
+        return output
+
+
+def triggerPlay(driver):
+    '''
+    Trigger current play status.
+
+    :param driver: WebDriver, the WebDriver to trigger
+    :returns: Bool, if the trigger is successful
+    '''
+    try:
+        videoPlayer = driver.find_element_by_class_name('video-js')
+        videoPlayer.click()
+        return True
+    except selenium.common.exceptions.NoSuchElementException:
+        return False
+
+
+def needAnswer(driver: selenium.webdriver.chrome.webdriver.WebDriver):
+    '''
+    Check if a question is shown.
+
+    :param driver: WebDriver, the WebDriver to check
+    :returns: Bool, if a question is shown.
+    '''
+    f = driver.find_elements_by_class_name('correctAnswer')
+    if(f):
+        return True
+    else:
+        return False
+
+
+def answer(driver: selenium.webdriver.chrome.webdriver.WebDriver):
+    '''
+    Answer in-video questions.
+
+    :param driver: WebDriver, the WebDriver to answer
+    :returns: Bool, if answer is successful
+    '''
+    try:
+        answers = driver.find_element_by_class_name(
+            'correctAnswer').get_attribute('data')
+        correctArray = [ord(i) - ord('A') for i in answers]
+        chooseName = 'gxb-icon-check'
+        try:
+            driver.find_element_by_class_name('gxb-icon-radio')
+            chooseName = 'gxb-icon-radio'
+        except selenium.common.exceptions.NoSuchElementException:
+            pass
+        for answer in correctArray:
+            driver.find_elements_by_class_name(chooseName)[
+                answer].click()
+        driver.find_element_by_class_name('submit').click()
+        play = WebDriverWait(driver, 2).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'player')))
+        play.click()
+        return True
+    except:
+        return False
+
+
+def nextVideo(driver: webdriver.chrome.webdriver.WebDriver):
+    match = re.match(CLASS_REGEX, driver.current_url)
+    if(not match):
+        return False
+    videoIds = list(map(lambda x: x.get_attribute(
+        'chapter_id'), videoList(driver)))
+    try:
+        # When the page is not video, append it to video list to get the nearest video.
+        if(match.groups()[2] not in videoIds):
+            videoIds.append(match.groups()[2])
+            videoIds.sort()
+        index = videoIds.index(match.groups()[2])
+        if(index != len(videoIds) - 1):
+            url = CLASS_STRING.format(
+                *match.groups()[:-1], videoIds[index + 1])
+            driver.get(url)
+            return True
+        else:
+            return False
+            # TODO: When the class ends. Raise a custom error and start a new class.
+    except ValueError:
+        return False
+
+
+def inVideoPage(driver: webdriver.chrome.webdriver.WebDriver):
+    match = re.match(CLASS_REGEX, driver.current_url)
+    if(not match):
+        return False
+    if(match.groups()[0] not in VLIDcache.keys()):
+        VLIDcache[match.groups()[0]] = list(map(lambda x: x.get_attribute(
+            'chapter_id'), videoList(driver)))
+    return(match.groups()[2] in VLIDcache[match.groups()[0]])
---
+++ console.py
@@ -0,0 +1,128 @@
+from selenium import webdriver
+import GxbManager as gm
+import json
+import threading
+import time
+import argparse
+GXB_LOGIN = '''https://cas.gaoxiaobang.com/login?tenant_id=1039&service=https%3A%2F%2Fbh3773.gaoxiaobang.com%2F'''
+helpString = '''GXB Manager Prototype
+GXB MANAGER IS ONLY FOR STUDY
+
+Command list:
+h: display help.
+s: show current status of video.
+p: play/stop current video.
+a: answer dialog question.
+n: jump to next video.
+q: quit the program.
+lg: login using username and password from user.json
+ls: get a list of all videos' title.
+ao: turn on auto class processer.
+af: turn off auto class processer.
+auto class processer can automatically solve in-video questions, start paused videos and skip discussions/exams.
+'''
+parser = argparse.ArgumentParser(description="Open GXB class manager")
+parser.add_argument("-f", "--file", default="user.json", help="json file for username and password")
+args = parser.parse_args()
+autoClassShouldStop = False
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--mute-audio')
+driver = webdriver.Chrome()
+driver.get('http://bh3773.gaoxiaobang.com')
+print('>>', end='')
+context = input()
+
+
+def autoClassProcess():
+    while(not autoClassShouldStop):
+        time.sleep(5)
+        # Ensure that the webpage is fully loaded.
+        try:
+            if(driver.execute_script('return document.readyState') != 'complete'):
+                continue
+            status = gm.status(driver)
+            if(not gm.inVideoPage(driver)):
+                if(gm.nextVideo(driver)):
+                    print(
+                        "[Notice] In exam or discuss page, forwarding to new video.")
+                else:
+                    print(
+                        "[Error] Can't find video and not in special pages! Ensure you are in a valid page!")
+            else:
+                if(status.playStatus == 'paused'):
+                    if(gm.needAnswer(driver)):
+                        if(gm.answer(driver)):
+                            print("[Notice] Auto answered in class question.")
+                        else:
+                            print("[Error] Video stopped for unknown reason!")
+                    else:
+                        if(gm.triggerPlay(driver)):
+                            print("[Notice] Auto played paused video.")
+                        else:
+                            print("[Error] Can't play paused video!")
+        except Exception as e:
+            print('[Warning] ', e)
+
+
+autoClass = threading.Thread(
+    target=autoClassProcess, name='AutoClassProcessingThread')
+
+while(context):
+    output = ""
+    if(context == 's'):
+        output = repr(gm.status(driver))
+    elif(context == 'h'):
+        output = helpString
+    elif(context == 'p'):
+        if(gm.triggerPlay(driver)):
+            output = ""
+        else:
+            output = "Not valid video page."
+    elif(context == 'a'):
+        if(gm.answer(driver)):
+            output = ""
+        else:
+            output = "Not valid time to answer."
+    elif(context == 'n'):
+        if(gm.nextVideo(driver)):
+            output = ""
+        else:
+            output = "Not valid video page or class reaches end."
+    elif(context == 'lg'):
+        username = None
+        password = None
+        try:
+            with open(args.file, 'r') as f:
+                djson = json.load(f)
+                username = djson['username']
+                password = djson['password']
+        except json.JSONDecodeError:
+            output = "Can't read user.json, login failed."
+        if(username and password and gm.autoLogin(driver, GXB_LOGIN, username, password)):
+            output = ""
+        else:
+            output = "Can't login. Maybe you have logged in?"
+    elif(context == 'ls'):
+        cl = gm.videoList(driver)
+        if(cl):
+            for i in cl:
+                print(i.get_attribute('title'))
+
+        else:
+            output = "Not valid class page."
+    elif(context == 'ao'):
+        if(autoClass.is_alive()):
+            output = "auto process already running!"
+        else:
+            autoClassShouldStop = False
+            autoClass.start()
+    elif(context == 'af'):
+        autoClassShouldStop = True
+    elif(context == 'q'):
+        break
+    else:
+        output = "Unknown command. Input h to see all available commands."
+    print(output)
+    print('>>', end='')
+    context = input()
+driver.quit()
